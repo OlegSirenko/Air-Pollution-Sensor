bf0f4e9a567dfb44e4609d04d1d9ade02ee5038c
Oleg Sirenko <spec_oleg@mail.ru>
2022-12-17 18:20:00 +0300

    Send some data over TCP socket

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import network  # библиотека для открытия точки доступа
 import gc  # сбощик мусора
+from machine import Pin
+import time
+import uselect as select
+
 
 try:
     import usocket as socket  # импортируем библиотеку сокета из Micropython
@@ -7,36 +11,55 @@ except ImportError:
     import socket
 
 
-def web_page():
-    html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
-              <body><h1>Hello, World!</h1></body></html>"""
-    return html
+def data_send():
+    data = b'1'
+    return data
+
+
+def data_collect():
+    print("Collecting...")
+    led = Pin(4, Pin.OUT)
+    led.on()
+    time.sleep(0.4)
+    led.off()
 
 
 gc.collect()  # Коллектим мусор
 
 ssid = 'Polution Sensor'
 
+
 ap = network.WLAN(network.AP_IF)  # режим работы антены вай-фая -- точка доступа
 ap.active(True)  # запускаем точку доступа
 ap.config(essid=ssid)  # Ставим название точки доступа
 
-print("Starting access point...", end="")
+print("Starting access point...")
 while not ap.active():  # пока точка доступа не запустится, сидим в цикле
     print(".", end="")
 
 print('\nAccess Point Started!')
-print(ap.ifconfig())
+print(ap.ifconfig())  # выводим информацию о точке доступа
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 80))
-s.listen(2)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # открываем  сокет, при подключении порт не нужен
+server.bind(("", 80))  # биндим его
+server.listen(1)  # ставим максимальное число подключившихся
+
+print("Wait for connection...")
 
 while True:
-    conn, addr = s.accept()
-    print('Got a connection from %s' % str(addr))
-    request = conn.recv(4096)
-    print('Content = %s' % str(request))
-    response = web_page()
-    conn.send(response)
-    conn.close()
+    r, w, err = select.select((server,), (), (), 1)
+    if r:
+        for readable in r:
+            conn, addr = server.accept()  # Нужно учитывать, что это блокирующая функция
+            print('Got a connection from %s' % str(addr))  # Принтим коннектион
+
+            data_collect()
+            request = conn.recvfrom(512)  # Указываем буфер УЧИТЫВАТЬ ЧТО ЭТО блокирующая функция
+            print('Content = %s' % str(request))  #
+            conn.send(b"Some data (Next it will be file)")  # Отправляем
+    else:
+        data_collect()
+
+server.close()
+
+
